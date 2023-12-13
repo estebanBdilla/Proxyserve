d732e315a0467d01cb8ac1534e0afe2d11516c7a
estebanBdilla <fbalcazar@3dadv.mx>
2023-12-12 18:05:24 -0600

    add proxy

---
+++ cors-config.ts
@@ -0,0 +1,4 @@
+export const allowedOrigins: string[] = [
+  'http://localhost:4200',
+  'http:/localhost:8081',
+];
---
+++ src/proxy.ts
@@ -0,0 +1,56 @@
+// proxy.ts
+
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    const allowedOrigins = [process.env.FRONTEND_URL];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+const backendProxy = createProxyMiddleware('/api', {
+  target: process.env.BACKEND_URL,
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '',
+  },
+});
+
+const frontendProxy = createProxyMiddleware('/', {
+  target: process.env.FRONTEND_URL,
+  changeOrigin: true,
+});
+
+app.use((req, res, next) => {
+  console.log(`Solicitud recibida: ${req.method} ${req.url}`);
+  next();
+});
+
+app.use('/api', backendProxy);
+app.use('/', frontendProxy);
+
+app.use((req, res, next) => {
+  res.on('close', () => {
+    console.log(`Respuesta enviada: ${res.statusCode}`);
+  });
+  next();
+});
+
+
+const port = process.env.PROXY_PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor de reenvío iniciado en http://localhost:${port}`);
+});
